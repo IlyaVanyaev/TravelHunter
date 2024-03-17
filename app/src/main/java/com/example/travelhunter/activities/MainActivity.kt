cfd7e1136a536c7e2167eee1b9888d6a010cf2c1
Ilya <IL4Van@yandex.ru>
2024-03-17 21:06:36 +0300

    added two Sign in and Registration fragments

--- app/src/main/java/com/example/travelhunter/activities/MainActivity.kt
+++ app/src/main/java/com/example/travelhunter/activities/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.travelhunter
+package com.example.travelhunter.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.travelhunter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
---
+++ app/src/main/java/com/example/travelhunter/fragments/Register.kt
@@ -0,0 +1,43 @@
+package com.example.travelhunter.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.travelhunter.R
+import com.example.travelhunter.databinding.FragmentRegisterBinding
+
+
+class Register : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.registerButton.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_register_to_signIn)}
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = Register()
+    }
+}
