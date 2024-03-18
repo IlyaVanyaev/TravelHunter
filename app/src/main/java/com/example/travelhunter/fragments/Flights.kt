03dc8a45fe44ff23b295dc23ae5c486849066546
Ilya <IL4Van@yandex.ru>
2024-03-19 01:32:17 +0300

    added date picker

--- app/src/main/java/com/example/travelhunter/fragments/Flights.kt
+++ app/src/main/java/com/example/travelhunter/fragments/Flights.kt
@@ -1,27 +1,73 @@
 package com.example.travelhunter.fragments
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.travelhunter.R
+import com.example.travelhunter.databinding.FragmentFlightsBinding
+import com.example.travelhunter.viewmodels.MainViewModel
+import android.icu.util.Calendar
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import com.example.travelhunter.activities.MainActivity
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.Year
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.util.Locale
 
 
 class Flights : Fragment() {
 
+    private lateinit var binding: FragmentFlightsBinding
+    //private val vm: MainViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //vm = ViewModelProvider(this)[MainViewModel::class.java]
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flights, container, false)
+    ): View {
+
+        binding = FragmentFlightsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.flightsToDate.setOnClickListener { datePicker(binding.flightsToDateText) }
+        binding.flightsFromDate.setOnClickListener { datePicker(binding.flightsFromDateText) }
+    }
+
+    private fun datePicker(textView: TextView) {
+        val datePicker = context?.let {
+            DatePickerDialog(
+                it, { DatePicker, year:Int, month:Int, day:Int ->
+
+                    val selectedDate = Calendar.getInstance()
+                    selectedDate.set(year, month, day)
+                    val dateFormat = android.icu.text.SimpleDateFormat("EEEE, dd/MM/yyyy", Locale.getDefault())
+                    val formattedDate = dateFormat.format(selectedDate.time)
+                    textView.text = formattedDate
+            },
+                Calendar.getInstance().get(java.util.Calendar.YEAR),
+                Calendar.getInstance().get(java.util.Calendar.MONTH),
+                Calendar.getInstance().get(java.util.Calendar.DAY_OF_MONTH)
+            )
+
+        }
+        datePicker?.show()
     }
 
     companion object {
--- app/src/main/java/com/example/travelhunter/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/travelhunter/viewmodels/MainViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.travelhunter.viewmodels
 
 import android.app.Application
+import android.app.DatePickerDialog
+import android.content.Context
+import android.icu.util.ULocale
+import android.widget.DatePicker
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.transition.Visibility
+import com.example.travelhunter.activities.MainActivity
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainViewModel (application: Application) : AndroidViewModel(application) {
 
@@ -12,7 +20,14 @@ class MainViewModel (application: Application) : AndroidViewModel(application) {
     val get_bottom_nav_visibility : LiveData<Boolean> = bottom_nav_visibility
 
 
+
+
+
     fun setBottomNavVisibility(visibility: Boolean){
         bottom_nav_visibility.value = visibility
     }
+
+
+
+
 }
